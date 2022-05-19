6f9bc4ec848d0eb91e694a12ce1291e78699bb82
mamun417 <abdullah.ssc13@gmail.com>
2022-05-20 02:06:06 +0600

    result dynamic

--- src/components/pages/Result.js
+++ src/components/pages/Result.js
@@ -1,12 +1,56 @@
 import Analysis from "../Analysis";
 import Summary from "../Summary";
+import useAnswers from "../../hooks/useAnswers";
+import { useHistory, useParams } from "react-router-dom";
+import _ from "lodash";
 
 export default function Result() {
-    console.log("show result");
+    const { location } = useHistory();
+    const { state } = location;
+    const { qna } = state;
+    const { id: videoId } = useParams();
+    const { loading, error, answers } = useAnswers(videoId);
+
+    function calculate() {
+        let score = 0;
+
+        answers.forEach((question, index1) => {
+            let correctIndexes = [],
+                checkedIndexes = [];
+
+            question.options.forEach((option, index2) => {
+                if (option.correct) {
+                    correctIndexes.push(index2);
+                }
+
+                if (qna[index1].options[index2].checked) {
+                    checkedIndexes.push(index2);
+                    option.checked = true;
+                }
+            });
+
+            if (_.isEqual(correctIndexes, checkedIndexes)) {
+                score = score + 5;
+                question.right_answer = true;
+            }
+        });
+
+        return score;
+    }
+
+    const userScore = calculate();
+
     return (
         <>
-            <Summary />
-            <Analysis />
+            {loading && <div>Loading...</div>}
+            {error && <div>There was an error!</div>}
+
+            {answers && answers.length > 0 && (
+                <>
+                    <Summary score={userScore} noq={answers.length} />
+                    <Analysis answers={answers} />
+                </>
+            )}
         </>
     );
 }
