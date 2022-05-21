2341a36fe75ba7dec796550fea6d355b1d33b942
mamun417 <abdullah.ssc13@gmail.com>
2022-05-21 15:51:27 +0600

    fix Route not used issue

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { BrowserRouter as Router, Switch } from "react-router-dom";
 import "../styles/App.css";
 import Home from "./pages/Home";
 import Login from "./pages/Login";
